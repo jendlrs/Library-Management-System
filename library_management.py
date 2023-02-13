7d93acd9949e309c03d984f78825b9b12cf3dbf1
unknown <jensennics.dlrs@gmail.com>
2023-02-14 00:45:39 +0800

    Completed the function of lending a book by using book's ID. Added condition to check if the book is available or not.

--- library_management.py
+++ library_management.py
@@ -22,7 +22,7 @@ class LMS:
             self.books_dict.update({str(Id):{ #Updating Dictionary
             "books_title":line.replace("\n", ""), 
             "lender_name": "",
-            "Issue_date": "", 
+            "issue_date": "", 
             "Status":"Available"}})
             Id = Id + 1
 
@@ -33,6 +33,25 @@ class LMS:
 
         for key, value in self.books_dict.items():
             print(key, "\t\t", value.get("books_title"), "- [", value.get("Status"),"]")
+
+    def Issue_books(self):
+        books_id = input ("Enter the book's ID: ")
+        current_date = datetime.datetime.now().strftime ("%Y-%m_%d %H:%M:%S")
+        if books_id in self.books_dict.keys():
+            if not self.books_dict[books_id]["Status"] == "Available":
+                print(f"This book is already issued to {self.books_dict[books_id]['lender_name']} \
+                    on {self.books_dict[books_id]['issue_date']}")
+                return self.Issue_books()
+            elif self.books_dict[books_id]['Status'] == "Available":
+                name = input ("Enter your name: ")
+                self.books_dict[books_id]['lender_name'] = name
+                self.books_dict[books_id]['issue_date'] = current_date
+                self.books_dict[books_id]['Status'] = "Already Issued"
+                print ("Books Issued Successfully! \n")
+
+        else:
+            print("Book ID not found!")
+            return self.Issue_books()
 
 l = LMS("List_of_books.txt", "Python's Library")
 print(l.display_books())

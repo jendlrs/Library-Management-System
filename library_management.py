ce95b2807a725ad0b875decfb2a4a4aafdf4cc23
unknown <jensennics.dlrs@gmail.com>
2023-02-14 00:27:29 +0800

    Updated the dictionary to use it easily and completed the function to display all the books.

--- library_management.py
+++ library_management.py
@@ -12,13 +12,27 @@ class LMS:
     def __init__(self, list_of_books, library_name):
         self.list_of_books = "list_of_books.txt"
         self.library_name = library_name
-        self.books_dict = {} #This will contain books title, lender name, issue date, and book status
+        self.books_dict = {} #This dictionary will contain books title, lender name, issue date, and book status
         Id = 101 #Starting ID
 
         #read the text file
         with open (self.list_of_books) as bk:
             content = bk.readlines()
         for line in content:
-            print (line)
+            self.books_dict.update({str(Id):{ #Updating Dictionary
+            "books_title":line.replace("\n", ""), 
+            "lender_name": "",
+            "Issue_date": "", 
+            "Status":"Available"}})
+            Id = Id + 1
 
-print (LMS("list-of_books.txt","Python's Library"))
+    def display_books(self):
+        print("------------------------List of Books---------------------")
+        print("Books ID", "\t", "Title")
+        print("----------------------------------------------------------")
+
+        for key, value in self.books_dict.items():
+            print(key, "\t\t", value.get("books_title"), "- [", value.get("Status"),"]")
+
+l = LMS("List_of_books.txt", "Python's Library")
+print(l.display_books())

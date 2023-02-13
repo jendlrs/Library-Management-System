b3ee286d236823061e7364dec43845d22d8f7708
unknown <jensennics.dlrs@gmail.com>
2023-02-14 01:44:28 +0800

    The code from the tutorial is completed. Last step I did is to add dictionary for the user to choose the functions they want to execute. My next commits will be my own modification to the sourcecode.

--- library_management.py
+++ library_management.py
@@ -64,10 +64,10 @@ class LMS:
             with open(self.list_of_books, "a") as bk:
                 bk.writelines(f"{new_books}\n")
                 self.books_dict.update({str(int(max(self.books_dict))+1):{
-                "books_title":new_books, 
-                "lender_name":"", 
-                "issue_date":"", 
-                "Status":"Available"}})
+                'books_title':new_books, 
+                'lender_name':"", 
+                'issue_date':"", 
+                'Status':"Available"}})
                 print (f"This books '{new_books}' has been added successfully!")
 
     def return_books(self):
@@ -85,11 +85,35 @@ class LMS:
         else:
             print("Book ID is not found")
 
-l = LMS("List_of_books.txt", "Python's Library")
-print(l.display_books())
-l.add_books()
-print(l.display_books())
-l.Issue_books()
-print(l.display_books())
-l.return_books()
-print(l.display_books())
+try:
+    myLMS = LMS("list_of_books.txt", "Python's")
+    press_key_dict = {"D": "Display Books",
+                    "I":"Issue Books",
+                    "A": "Add Books", 
+                    "R": "Return Books",
+                    "Q": "Quit"}    
+    key_press = False
+    while not (key_press == "q"):
+        print(f"\n----------Welcome To {myLMS.library_name}'s Library Management System---------\n")
+        for key, value in press_key_dict.items():
+            print("Press", key, "To", value)
+        key_press = input ("Press key: ").lower()
+        if key_press == "i":
+            print("\nCurrent Selection: Issue Books\n")
+            myLMS.Issue_books()
+        elif key_press == 'd':
+            print("\nCurrent Selection: Display Books\n")
+            myLMS.display_books()
+        elif key_press == 'a':
+            print("\nCurrent Selection: Add Books\n")
+            myLMS.add_books()
+        elif key_press == 'r':
+            print("\nCurrent Selection: Return Books\n")
+            myLMS.return_books()
+        elif key_press == 'q':
+            break
+        else:
+            continue    
+
+except Exception as e:
+    print("Something went wrong. Please check your input")

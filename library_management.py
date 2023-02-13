7cdccb734adb2af8a0d376e6ad0288ed286d6a31
unknown <jensennics.dlrs@gmail.com>
2023-02-14 00:04:42 +0800

    Created class that will contain all the modules needed. Also added constructor, defined needed variables for useful information and added code to read the text file.

--- library_management.py
+++ library_management.py
@@ -3,4 +3,22 @@
 #Importing Libraries
 import datetime
 import os
-os.getcwd()
+#os.getcwd()
+
+class LMS:
+    """ This class is used to keep record of books library
+    It has total four module: "Display book, "Issue Books", "Return Books", "Add Books" """
+
+    def __init__(self, list_of_books, library_name):
+        self.list_of_books = "list_of_books.txt"
+        self.library_name = library_name
+        self.books_dict = {} #This will contain books title, lender name, issue date, and book status
+        Id = 101 #Starting ID
+
+        #read the text file
+        with open (self.list_of_books) as bk:
+            content = bk.readlines()
+        for line in content:
+            print (line)
+
+print (LMS("list-of_books.txt","Python's Library"))
